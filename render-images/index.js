0a838f31bb86583c92ce5080ebcae74dee55b973
mehajabingit99 <102028765+mehajabingit99@users.noreply.github.com>
2023-01-18 19:24:35 +0530

    initial render images files

---
+++ render-images/index.js
@@ -0,0 +1,15 @@
+const imgs =[
+    "images/hip1.jpg",
+    "images/hip2.jpg",
+    "images/hip3.jpg",
+]
+const container = document.getElementById("container")
+function renderImages(){
+    var imgsDOM = ""
+    for( var i=0; i<imgs.length; i++){
+        imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`
+
+    }
+    container.innerHTML = imgsDOM
+}
+ renderImages()
